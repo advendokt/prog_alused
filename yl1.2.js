3c95e4ae2f41bd0c6929583f69ffa41a10bfa1d7
Maksim <maksim.murk@voco.ee>
2024-09-26 13:41:00 +0300

    yl1.2.js

---
+++ yl1.2.js
@@ -0,0 +1,6 @@
+
+var aasta = "2016.";
+var president = " Kersti Kaljulaid";
+var LauseKesosa = " aastal valiti Eesti presidendiks";
+let lause = aasta + LauseKesosa + president;
+console.log(lause)
